d3b4aa45c7cb9d5698a954e53875c27247afd804
Prethush <ktmirash49@.gmail.com>
2021-10-07 10:40:02 +0530

    stack using object

--- block-BJaalb/code/index.js
+++ block-BJaalb/code/index.js
@@ -1,24 +1,69 @@
+// class Stack {
+//   constructor(capacity = Infinity) {
+//     this.storage = [];
+//     this.capacity = capacity;
+//   }
+//   get length() {
+//     return this.storage.length;
+//   }
+
+//   add(num) {
+//     if(this.storage.length === this.capacity) {
+//       alert("Stack overflow");
+//     }else {
+//       this.storage.push(num);
+//        return this.storage.length;
+//     }
+    
+//   }
+
+//   remove() {
+//     return this.storage.pop();
+//   }
+
+//   peek() {
+//     return this.storage[this.length - 1];
+//   }
+
+//   printAll() {
+//     for(let i = this.length - 1; i >= 0; i--) {
+//       console.log(this.storage[i]);
+//     } 
+//   }
+
+//   isEmpty() {
+//     return this.storage.length === 0 ? true : false;
+//   }
+
+// }
+
+// const stack = new Stack();
+// const stack2 = new Stack(4);
+
 class Stack {
   constructor(capacity = Infinity) {
-    this.storage = [];
+    this.storage = {},
     this.capacity = capacity;
   }
+
   get length() {
-    return this.storage.length;
+    return Object.keys(this.storage).length;
   }
 
-  add(num) {
-    if(this.storage.length === this.capacity) {
-      alert("Stack overflow");
+  add(value) {
+    let lastIndex = this.length;
+    if(this.length === this.capacity) {
+      alert("Stack overflowing");
     }else {
-      this.storage.push(num);
-       return this.storage.length;
+      this.storage[lastIndex] = value;
+      return Object.keys(this.storage).length;
     }
-    
   }
 
   remove() {
-    return this.storage.pop();
+    let elm = this.storage[this.length - 1];
+    delete this.storage[this.length - 1];
+    return elm;
   }
 
   peek() {
@@ -26,15 +71,14 @@ class Stack {
   }
 
   printAll() {
-    for(let i = this.length - 1; i >= 0; i--) {
-      console.log(this.storage[i]);
-    } 
+   for(let i = this.length; i >= 0; i--) {
+     console.log(this.storage[i]);
+   }
   }
 
   isEmpty() {
-    return this.storage.length === 0 ? true : false;
+    return Object.keys(this.storage).length === 0 ? true : false;
   }
-
 }
 
 // const stack = new Stack();
